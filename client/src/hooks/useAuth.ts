c5dcae8b6521b0f89845c703f67b4f11837cc39d
kristina <kristina@luday.se>
2025-05-10 22:38:37 +0200

    details user interface

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -2,21 +2,18 @@
 import { useState, useEffect } from 'react';
 import { getProfile, User} from '../api/users';
 
-export type AuthUser = Omit<User, 'email'>;
+// export type AuthUser = Omit<User, 'email'>;
 
 export function useAuth() {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getProfile()
+   getProfile()
       .then(profile => {
-        // we only need id, name, shareId here
-        const { id, name, shareId } = profile;
-        setUser({ id, name, shareId });
+        setUser(profile);   
       })
       .catch(err => {
-        // if 401 or other error, treat as not-logged-in
         console.error('Auth error:', err);
         setUser(null);
       })
