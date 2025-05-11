8e1f690946521cb5014c00ed125bfb5ddc9b8f53
kristina <kristina@luday.se>
2025-05-11 16:03:56 +0200

    app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,14 @@
 // src/main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
-import { SocketProvider }  from './context/SocketProvider';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { SocketProvider } from "./context/SocketProvider";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <SocketProvider>
-          <App />
-      </SocketProvider>
-    </HashRouter>
+    <SocketProvider>
+      <App />
+    </SocketProvider>
   </React.StrictMode>
 );
